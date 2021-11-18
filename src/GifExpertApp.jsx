15223e130b795b90a81815d87ee423bd6587067c
AnaRativa09 <anarativa09@gmail.com>
2021-11-18 18:13:45 -0500

    first commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid'
+
+const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch']);
+
+  /* const handleAdd = () => {
+      setCategories([...categories, 'HunterXHunter']);
+      setCategories((cats) => [...cats, 'HunterXHunter']); // Callback para cuando no accedemos al estado directamente
+    }*/
+
+  return (
+    <>
+      <h2>Gif Expert App</h2>
+      <AddCategory setCategories={setCategories}/>
+      <hr />
+
+      <ol>
+        {
+          categories.map((category) => (
+            <GifGrid
+              key={category}
+              category={category}
+            />
+          ))
+        }
+      </ol>
+    </>
+  )
+}
+
+export default GifExpertApp;
